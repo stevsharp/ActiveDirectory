d0016e000b828dd0c48d221fd00bbb4c58d17902
Spiros <Spyros_Ponaris@hotmail.com>
2018-10-16 19:18:36 +0300

    Commit

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 
 namespace ConsoleApp6
 {
+    /// <summary>
+    /// Install-Package System.DirectoryServices.AccountManagemen 
+    /// </summary>
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using (var ctx = new PrincipalContext(ContextType.Domain, "MyDomain"))
+                {
+                    var myDomainUsers = new List<string>();
+                    var userPrinciple = new UserPrincipal(ctx);
+                    using (var search = new PrincipalSearcher(userPrinciple))
+                    {
+                        foreach (var domainUser in search.FindAll())
+                        {
+                            if (domainUser.DisplayName != null)
+                            {
+                                myDomainUsers.Add(domainUser.DisplayName);
+                            }
+                        }
+                    }
+
+                    foreach (var item in myDomainUsers)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            Console.ReadLine();
         }
     }
 }
