16354fb94e9c985a4b4bae1f3e5b72ec4dd18277
Spiros <Spyros_Ponaris@hotmail.com>
2018-10-20 18:16:55 +0300

    LdapConnection

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Novell.Directory.Ldap;
+using System;
 using System.Collections.Generic;
 using System.DirectoryServices.AccountManagement;
 
@@ -14,27 +15,32 @@ namespace ConsoleApp6
         {
             try
             {
-                using (var ctx = new PrincipalContext(ContextType.Domain, "MyDomain"))
-                {
-                    var myDomainUsers = new List<string>();
-                    var userPrinciple = new UserPrincipal(ctx);
-                    using (var search = new PrincipalSearcher(userPrinciple))
-                    {
-                        foreach (var domainUser in search.FindAll())
-                        {
-                            if (domainUser.DisplayName != null)
-                            {
-                                myDomainUsers.Add(domainUser.DisplayName);
-                            }
-                        }
-                    }
+                #region comment
+                //using (var ctx = new PrincipalContext(ContextType.Domain, "MyDomain"))
+                //{
+                //    var myDomainUsers = new List<string>();
+                //    var userPrinciple = new UserPrincipal(ctx);
+                //    using (var search = new PrincipalSearcher(userPrinciple))
+                //    {
+                //        foreach (var domainUser in search.FindAll())
+                //        {
+                //            if (domainUser.DisplayName != null)
+                //            {
+                //                myDomainUsers.Add(domainUser.DisplayName);
+                //            }
+                //        }
+                //    }
 
-                    foreach (var item in myDomainUsers)
-                    {
-                        Console.WriteLine(item);
-                    }
+                //    foreach (var item in myDomainUsers)
+                //    {
+                //        Console.WriteLine(item);
+                //    }
 
-                }
+                //}
+                #endregion
+
+                var canLogin = ValidateUser("SarakakisDomain", "UserName", "Password");
+                Console.WriteLine(canLogin);
             }
             catch (Exception)
             {
@@ -43,5 +49,29 @@ namespace ConsoleApp6
 
             Console.ReadLine();
         }
+
+        public static bool ValidateUser(string domainName, string username, string password)
+        {
+            string userDn = $"{username}@{domainName}";
+
+            try
+            {
+                using (var connection = new LdapConnection { SecureSocketLayer = false })
+                {
+                    connection.Connect(domainName, LdapConnection.DEFAULT_PORT);
+                    connection.Bind(userDn, password);
+
+                    if (connection.Bound)
+                        return true;
+                }
+            }
+            catch (LdapException ex)
+            {
+                // Log exception
+            }
+            return false;
+        }
     }
+
+}
 }
